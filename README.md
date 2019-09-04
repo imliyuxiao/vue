264ebadd8182da1bee80ab9521a290c89a436943
liyuxiao <imliyuxiao@qq.com>
2019-09-04 15:39:58 +0800

    关于项目信息
    添加vue代码，并说明项目的目的

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 # vue
- For testing out GitHub Desktop
-test
+ 在textarea中只能输入数字和逗号‘，’，过滤重复电话号码，对电话号码数组做验证
---
+++ phone.vue
@@ -0,0 +1,91 @@
+<template>
+    <div>
+        <Input v-model="formVideo.phone" type="textarea" class="textInput"
+               placeholder="多个手机号码用逗号（英文）隔开" @keyup.native="inputChange($event)"></Input>
+        <div class="send_tips">
+            共计<span style="padding:0 3px;">{{ formVideo.userCount }}</span>个有效号码，已过滤重复号
+
+            码
+        </div>
+        <span>{{formVideo.textTips}}</span>
+    </div>
+</template>
+<script>
+    export default {
+        data () {
+            return {
+                formVideo:{
+                    phone:'',
+                    userCount:0,
+                    textTips:''
+                },
+            }
+        },
+        methods: {
+            //输入手机号码
+            inputChange(){
+                let _this=this
+                var phoneStr = _this.formVideo.phone.replace(/[^\d\,]/g,'') //输入框只能输入数字和逗号
+                var phoneArr = phoneStr.split(",")
+                var newArr = [];
+                var rightPhone=[]
+                var i = ''
+
+                if(phoneStr){
+                    _this.btnRead=true
+                }else{
+                    _this.btnRead=false
+                }
+
+                //判断重复号码
+                //for (var i = 0; i<phoneArr.length ; i++) {
+                //如果temp中没有arr[i],则把它加入到temp中
+                //   if(phoneArr[i] ===''||phoneArr[i] ==undefined) {
+                //      phoneArr.splice(i,1);
+                //i = i - 1;
+                //    console.log('phoneArr::',phoneArr)
+                //  }
+                // if(newArr.indexOf(phoneArr[i])===-1){
+                //    newArr.push(phoneArr[i]);
+                //     console.log('newArr::',newArr)
+                //
+                //  _this.formVideo.phone=''
+                //  _this.formVideo.phone = newArr.join(',')
+                //  }
+                // }
+                newArr= [...new Set(phoneArr)];
+                console.log('newArr:',newArr)
+                _this.formVideo.phone=newArr.join(',')
+                newArr.map(function(item,index){
+                    //遍历判断号码有效性
+                    //var reg = 11 && /^((13|14|15|16|17|18|19)[0-9]{1}\d{8})$/
+                    var reg= /^(13[0-9]|14[5|7]|15[0|1|2|3|5|6|7|8|9]|18[0|1|2|3|5|6|7|8|9])\d{8}$/
+
+                    if (!reg.test(item)) {
+                        _this.errorTipsClass='errorTipShow'
+                        _this.formVideo.textTips="请输入正确的手机号码"
+
+                        //  if(newArr.length>1){
+                        //    _this.formVideo.userCount =index+1  //号码有效数
+                        //  }else{
+                        //    _this.formVideo.userCount = 0
+                        // }
+                    }else{
+                        console.log('item::',item)
+                        console.log('index::',index)
+                        _this.formVideo.userCount = index+1
+                        _this.errorTipsClass='errorTipsHide'
+                        _this.formVideo.textTips=""
+                    }
+                })
+            }
+        },
+        mounted () {
+
+        }
+    }
+</script>
+<style>
+
+</style>
+1
